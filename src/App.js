559fdc467c3cd683bc2448c6ce14691a4712d3c8
Merve Besen <mervebesenn@gmail.com>
2022-05-29 19:20:29 +0300

    çalışan hali

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Col, Container, Row } from "reactstrap";
 import Products from "./Products";
 import ShopList from "./ShopList";
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import çay from "./pictures/çay.jpg";
 import peynir from "./pictures/peynir.jpg";
@@ -20,44 +20,39 @@ import salam from "./pictures/salam.jpg";
 import sos from "./pictures/sos.jpg";
 import süt from "./pictures/süt.jpg";
 
-       
-
 export default class App extends Component {
-
-  productList = [
-    { id: 1, image: çay, title: "Çay", amount: 3.5 },
-    { id: 2, image: peynir, title: "Peynir", amount: 3.5 },
-    { id: 3, image: reçel, title: "Reçel", amount: 3.5 },
-    { id: 4, image: simit, title: "Simit", amount: 3.5 },
-    { id: 5, image: su, title: "Su", amount: 3.5 },
-    { id: 6, image: zeytin, title: "Zeytin", amount: 3.5 },
-    { id: 7, image: yumurta, title: "Yumurta", amount: 3.5 },
-    { id: 8, image: ekmek, title: "Ekmek", amount: 3.5 },
-    { id: 9, image: poğaça, title: "Poğaça", amount: 3.5 },
-    { id: 10, image: bal, title: "Bal", amount: 3.5 },
-    { id: 11, image: kaşar, title: "Kaşar", amount: 3.5 },
-    { id: 12, image: pekmez, title: "Pekmez", amount: 3.5 },
-    { id: 13, image: salam, title: "Salam", amount: 3.5 },
-    { id: 14, image: sos, title: "Sos", amount: 3.5 },
-    { id: 15, image: süt, title: "Süt", amount: 3.5 },
-  ]
-
   constructor(props) {
     super(props);
     this.state = {
-      
+      productList: [
+        { id: 1, image: çay, title: "Çay", amount: 3.5 },
+        { id: 2, image: peynir, title: "Peynir", amount: 3.5 },
+        { id: 3, image: reçel, title: "Reçel", amount: 3.5 },
+        { id: 4, image: simit, title: "Simit", amount: 3.5 },
+        { id: 5, image: su, title: "Su", amount: 3.5 },
+        { id: 6, image: zeytin, title: "Zeytin", amount: 3.5 },
+        { id: 7, image: yumurta, title: "Yumurta", amount: 3.5 },
+        { id: 8, image: ekmek, title: "Ekmek", amount: 3.5 },
+        { id: 9, image: poğaça, title: "Poğaça", amount: 3.5 },
+        { id: 10, image: bal, title: "Bal", amount: 3.5 },
+        { id: 11, image: kaşar, title: "Kaşar", amount: 3.5 },
+        { id: 12, image: pekmez, title: "Pekmez", amount: 3.5 },
+        { id: 13, image: salam, title: "Salam", amount: 3.5 },
+        { id: 14, image: sos, title: "Sos", amount: 3.5 },
+        { id: 15, image: süt, title: "Süt", amount: 3.5 },
+      ],
     };
   }
-  render(){
+  render() {
     return (
       <div>
         <Container>
           <Row>
             <Col xs="5">
-              <Products title="Product List" />
+              <Products title="Product List" productList={this.productList} />
             </Col>
             <Col xs="7">
-              <ShopList title="Shop List" />
+              <ShopList title="Shop List" productList={this.productList} />
             </Col>
           </Row>
           <Row></Row>
@@ -66,7 +61,6 @@ export default class App extends Component {
     );
   }
 }
-
 
 /*
 import çay from "./pictures/çay.jpg";
@@ -303,4 +297,3 @@ function App() {
   );
 }
 */
-
--- src/ShopList.js
+++ src/ShopList.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem ,Col, Container, Row} from "reactstrap";
-import productList from "./App"
+
+import çay from "./pictures/çay.jpg";
+import peynir from "./pictures/peynir.jpg";
+import reçel from "./pictures/reçel.jpg";
+import simit from "./pictures/simit.jpg";
+import su from "./pictures/su.jpg";
+import zeytin from "./pictures/zeytin.jpg";
+import yumurta from "./pictures/egg.jpeg";
+import ekmek from "./pictures/ekmek.jpeg";
+import poğaça from "./pictures/poğaça.jpeg";
+import bal from "./pictures/bal.jpg";
+import kaşar from "./pictures/kaşar.jpg";
+import pekmez from "./pictures/pekmez.jpg";
+import salam from "./pictures/salam.jpg";
+import sos from "./pictures/sos.jpg";
+import süt from "./pictures/süt.jpg";
 
 const toplamTutar=0;
 
 export default class ShopList extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: [
+        { id: 1, image: çay, title: "Çay", amount: 3.5 },
+        { id: 2, image: peynir, title: "Peynir", amount: 3.5 },
+        { id: 3, image: reçel, title: "Reçel", amount: 3.5 },
+        { id: 4, image: simit, title: "Simit", amount: 3.5 },
+        { id: 5, image: su, title: "Su", amount: 3.5 },
+        { id: 6, image: zeytin, title: "Zeytin", amount: 3.5 },
+        { id: 7, image: yumurta, title: "Yumurta", amount: 3.5 },
+        { id: 8, image: ekmek, title: "Ekmek", amount: 3.5 },
+        { id: 9, image: poğaça, title: "Poğaça", amount: 3.5 },
+        { id: 10, image: bal, title: "Bal", amount: 3.5 },
+        { id: 11, image: kaşar, title: "Kaşar", amount: 3.5 },
+        { id: 12, image: pekmez, title: "Pekmez", amount: 3.5 },
+        { id: 13, image: salam, title: "Salam", amount: 3.5 },
+        { id: 14, image: sos, title: "Sos", amount: 3.5 },
+        { id: 15, image: süt, title: "Süt", amount: 3.5 },
+      ],
+    };
+  }
   render() {
     return (
       <div>
@@ -14,7 +50,7 @@ export default class ShopList extends Component {
             <Row>
                 <Col> Product
         <ListGroup>
-          {productList.map((product) => (
+          {this.state.productList.map((product) => (
             <ListGroupItem key={product.id}>
               {product.title}
             </ListGroupItem>
@@ -23,7 +59,7 @@ export default class ShopList extends Component {
         </Col>
         <Col> Price
         <ListGroup>
-          {productList.map((product) => (
+          {this.state.productList.map((product) => (
             <ListGroupItem key={product.id}>
               {product.amount}
             </ListGroupItem>
